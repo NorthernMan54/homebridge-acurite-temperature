9344fa33e0f83c16d4557ae8f93c6e49440fe734
NorthernMan54 <buyminivan-northern@yahoo.ca>
2018-06-01 21:32:14 -0400

    Working

--- sensor.js
+++ sensor.js
@@ -10,6 +10,7 @@ var hostname = os.hostname();
 let Service, Characteristic;
 var CustomCharacteristic;
 var FakeGatoHistoryService;
+var refresh;
 
 var myAccessories = [];
 
@@ -27,10 +28,11 @@ function AcuritePlugin(log, config, api) {
   this.log = log;
   this.name1 = config.name1;
   this.name2 = config.name2;
-  this.refresh = config['refresh'] || 60; // Update every minute
+  refresh = config['refresh'] || 60; // Update every minute
   this.options = config.options || {};
   this.storage = config['storage'] || "fs";
   this.spreadsheetId = config['spreadsheetId'];
+  this.devices = config['devices'];
   if (this.spreadsheetId) {
     this.log_event_counter = 59;
     this.logger = new logger(this.spreadsheetId);
@@ -41,31 +43,28 @@ function AcuritePlugin(log, config, api) {
 
 AcuritePlugin.prototype = {
   accessories: function(callback) {
-    this.log("accessories");
-    myAccessories.push(new acuriteAccessory(this.name1,this.log));
-    myAccessories.push(new acuriteAccessory(this.name2,this.log));
+    for (var i in this.devices) {
+      this.log("Adding device", i, this.devices[i]);
+      myAccessories.push(new acuriteAccessory(this.devices[i], this.log, i));
+    }
     callback(myAccessories);
-
     var proc = spawn('/usr/local/bin/rtl_433', ['-q', '-G']);
     var start;
 
     proc.stdout.on('data', function(message) {
-        // {"time" : "2018-05-29 22:12:30", "model" : "Philips outdoor temperature sensor", "channel" : 1, "temperature_C" : 8.300, "battery" : "OK"}
-        this.log(message.toString());
 
         var data = message.toString().split(" ");
 
-        //this.log("Data",data);
-
-        // [ '2018-05-31',  '23:15:21',  'Acurite',  '986',  'sensor',  '0xc8e0',  '-',  '2F:',  '-17.2',  'C',  '1',
-
-        this.log(data[2],data[7],data[8]);
-        if ( data[2] == "Acurite") {
-          if ( data[7] == "1R:" ) {
-            myAccessories[0].updateStatus(data[8]);
-          } else {
-            myAccessories[1].updateStatus(data[8]);
-          }
+        switch (deviceModel(data)) {
+          case "Acurite":
+            // [ '2018-05-31',  '23:15:21',  'Acurite',  '986',  'sensor',  '0xc8e0',  '-',  '2F:',  '-17.2',  'C',  '1',
+            var unit = data[7];
+            var temperature = data[8];
+            var battery = "OK";
+            getDevice(unit).updateStatus(temperature, battery);
+            break;
+          default:
+            this.log("Undefined device", message.toString());
         }
 
       }.bind(this)
@@ -80,92 +79,34 @@ AcuritePlugin.prototype = {
   }
 }
 
-
-class AcuritePluginOld {
-  constructor(log, config, api) {
-    this.log = log;
-    this.name = config.name;
-    this.name_temperature = config.name_temperature || this.name;
-    this.name_humidity = config.name_humidity || this.name;
-    this.refresh = config['refresh'] || 60; // Update every minute
-    this.options = config.options || {};
-    this.storage = config['storage'] || "fs";
-    this.spreadsheetId = config['spreadsheetId'];
-    if (this.spreadsheetId) {
-      this.log_event_counter = 59;
-      this.logger = new logger(this.spreadsheetId);
-    }
-    this.lastUpdated = Date.now();
-
-    this.informationService = new Service.AccessoryInformation();
-
-    this.informationService
-      .setCharacteristic(Characteristic.Manufacturer, "acurite-Temperature")
-      .setCharacteristic(Characteristic.SerialNumber, hostname + "-" + this.name_temperature)
-      .setCharacteristic(Characteristic.FirmwareRevision, require('./package.json').version);
-
-    this.temperatureService = new Service.TemperatureSensor(this.name_temperature);
-
-    this.temperatureService
-      .getCharacteristic(Characteristic.CurrentTemperature)
-      .setProps({
-        minValue: -100,
-        maxValue: 100
-      });
-
-    setInterval(this.heartbeat.bind(this), this.refresh * 1000);
-
-    this.temperatureService.log = this.log;
-    this.loggingService = new FakeGatoHistoryService("weather", this.temperatureService, {
-      storage: this.storage,
-      minutes: this.refresh * 10 / 60
-    });
-
-  }
-
-  heartbeat() {
-    if (Date.now() - this.lastUpdated > 15 * 60 * 1000) // Alert after 15 minutes of no Data
-    {
-      this.log.error("No response from sensor");
-      this.temperatureService
-        .getCharacteristic(Characteristic.CurrentTemperature).updateValue(new Error("No response"));
-    }
-  }
-
-  getServices() {
-    return [this.informationService, this.temperatureService, this.loggingService]
-  }
-}
-
-function acuriteAccessory(name,log) {
-  console.log("THIS",this);
+function acuriteAccessory(name, log, unit) {
+  this.unit = unit;
   this.log = log;
   this.name = name;
 }
 
 acuriteAccessory.prototype = {
-
-  updateStatus: function(data) {
+  updateStatus: function(temperature, battery) {
     try {
-      this.log("Message", data);
-
-
+      this.log("Updating", this.name, temperature, battery);
       this.lastUpdated = Date.now();
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(deviceTimeout.bind(this), refresh * 1000);
       this.loggingService.addEntry({
         time: moment().unix(),
-        temp: roundInt(data)
+        temp: roundInt(temperature)
       });
 
       if (this.spreadsheetId) {
         this.log_event_counter = this.log_event_counter + 1;
         if (this.log_event_counter > 59) {
-          this.logger.storeBME(this.name, 0, roundInt(data));
+          this.logger.storeBME(this.name, 0, roundInt(temperature));
           this.log_event_counter = 0;
         }
       }
       this.temperatureService
-        .setCharacteristic(Characteristic.CurrentTemperature, roundInt(data));
-      if (data.battery != "OK") {
+        .setCharacteristic(Characteristic.CurrentTemperature, roundInt(temperature));
+      if (battery == "OK") {
         this.temperatureService
           .setCharacteristic(Characteristic.StatusLowBattery, Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL);
       } else {
@@ -196,20 +137,37 @@ acuriteAccessory.prototype = {
         maxValue: 100
       });
 
-    //setInterval(this.heartbeat.bind(this), this.refresh * 1000);
+    this.timeout = setTimeout(deviceTimeout.bind(this), refresh * 1000);
 
     this.temperatureService.log = this.log;
     this.loggingService = new FakeGatoHistoryService("weather", this.temperatureService, {
       storage: this.storage,
-      minutes: this.refresh * 10 / 60
+      minutes: refresh * 10 / 60
     });
 
     return [informationService, this.temperatureService, this.loggingService];
-
   }
 }
 
+function deviceTimeout() {
+  this.log("Timeout",this.name);
+  this.temperatureService
+    .getCharacteristic(Characteristic.CurrentTemperature).updateValue(new Error("No response"));
+}
+
+function deviceModel(data) {
+  if (data[2] == "Acurite")
+    return "Acurite";
+}
 
 function roundInt(string) {
   return Math.round(parseFloat(string) * 10) / 10;
+}
+
+function getDevice(unit) {
+  for (var i in myAccessories) {
+    if ( myAccessories[i].unit == unit )
+      return myAccessories[i];
+  }
+  this.log.error("ERROR: unknown unit -",unit);
 }
